81e96f345002dc4e282ff2150d4d2ce6d742e64e
elharony <p.yahyaelharony@gmail.com>
2018-07-18 13:57:47 +0200

    Implement the 'Search' feature in the sidebar menu

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react'
 import './Menu.css'
+import escapeRegExp from 'escape-string-regexp'
 
+// Places Data
 import places from '../../data/places.json'
 
 class Menu extends Component {
+
+    state = {
+        query: '',
+        places: places
+    }
+
+    updateQuery = (query) => {
+        this.setState({ query })
+    }
+
     render() {
+
+        const { query, places } = this.state
+        let filteredPlaces
+
+        if(query) {
+            const match = new RegExp(escapeRegExp(query), 'i');
+            filteredPlaces = places.filter((place) => match.test(place.place_title))
+        } else {
+            filteredPlaces = places
+        }
+
         return (
             <aside>
                 <h2>Find A Place!</h2>
                 <div className="search-form">
-                    <input type="text" placeholder="Search Here"/>
+                    <input type="text" placeholder="Search Here" onChange={(e) => this.updateQuery(e.target.value)} value={this.state.query}/>
                 </div>
                 <ul className="search-result">
-                    {places.map((place) => (
+                    {filteredPlaces.map((place) => (
                         <li className="item">{place.place_title}</li>
                     ))}
                 </ul>
